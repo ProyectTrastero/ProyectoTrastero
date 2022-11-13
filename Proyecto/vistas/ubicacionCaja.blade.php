2ae3a2d5626c86009653400081d06ece98a1b2a3
ecovb <101084037+ecovb@users.noreply.github.com>
2022-11-13 20:47:07 +0100

    actualizado con dani

---
+++ Proyecto/vistas/ubicacionCaja.blade.php
@@ -0,0 +1,36 @@
+{{-- Usamos la vista app como plantilla --}}
+@extends('app')
+
+{{-- Sección aporta el título de la página --}}
+@section('title', 'Ubicacion Caja')
+
+{{-- Sección mensaje --}}
+@section('content')
+<script src="asset/js/ubicacionCaja.js"></script>
+<form action="" method="POST">     
+    <div class="container">
+        <div>Seleccione la ubicación de la caja:</div>
+        <div>
+            <label for="estanteria">Estantería</label>
+            <select id="seleccionEstanteria" name="estanteria">
+                @foreach($estanterias as $clave=>$valor)
+                <option>{{$clave+1}}</option>
+                @endforeach
+            </select>
+        </div>
+        <div>
+            <label for="balda">Balda</label>
+            <select id="seleccionBalda" name="balda">
+                
+            </select>  
+        </div>
+        <div>
+            <label><input id="sinAsignar" type="checkbox" name="sinAsignar">Sin asignar</label>
+        </div>
+        <div>
+            <input class="btn btn-info" type="submit" name="volver" value="Volver">
+            <input class="btn btn-info" type="submit" name="añadirUbicacion" value="Añadir">
+        </div>
+    </div>
+</form>
+@endsection
