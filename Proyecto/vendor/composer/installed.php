c57555f7626a2480786d9b7c2b2c54596f4861d9
martafonfreda <66180644+martafonfreda@users.noreply.github.com>
2022-11-13 21:29:47 +0100

    actualizacion marta

--- Proyecto/vendor/composer/installed.php
+++ Proyecto/vendor/composer/installed.php
@@ -39,15 +39,6 @@
             'reference' => '8b6386d7417526d1ea4da9edb70b8352f7543627',
             'dev_requirement' => false,
         ),
-        'phpmailer/phpmailer' => array(
-            'pretty_version' => 'v6.6.5',
-            'version' => '6.6.5.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
-            'aliases' => array(),
-            'reference' => '8b6386d7417526d1ea4da9edb70b8352f7543627',
-            'dev_requirement' => false,
-        ),
         'phpoption/phpoption' => array(
             'pretty_version' => '1.9.0',
             'version' => '1.9.0.0',
