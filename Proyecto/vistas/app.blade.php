8e80616b099c6d7a028f09ce0a6a1112436979d3
thedezz360 <thedezz360@gmail.com>
2022-11-26 15:32:16 +0100

    editar perfil

--- Proyecto/vistas/app.blade.php
+++ Proyecto/vistas/app.blade.php
@@ -4,9 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title')</title>
-        <!--<link rel="stylesheet" href="asset/css/style.css" >-->
         <link rel="stylesheet" href="asset/css/bootstrap/bootstrap.min.css">
         <link rel="stylesheet" href="asset/css/all.css">
+        <link rel="stylesheet" href="asset/css/style.css" >
         
     </head>
      <body>
