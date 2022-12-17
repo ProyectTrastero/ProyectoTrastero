32ffedfd87995df4cffe924c9b520c4c63524208
ecovb <101084037+ecovb@users.noreply.github.com>
2022-12-17 21:52:12 +0100

    actualizacion emma

--- Proyecto/vistas/añadirTrastero.blade.php
+++ Proyecto/vistas/añadirTrastero.blade.php
@@ -200,7 +200,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="staticBackdropLabel">Fallo al guardar: </h1>
+                <h1 class="modal-title fs-5" id="staticBackdropLabel"></h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
